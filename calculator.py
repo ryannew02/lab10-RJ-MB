cfaa15904cbae7c22451c32ee40cd6804547b279
Ryan Juergens <ryanjuergens@mail.com>
2025-11-10 19:30:02 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -8,6 +8,15 @@ import math
 # First example
 import math
 
+def square_root(a):
+	try:
+		return math.sqrt(a)
+	except:
+		raise ValueError
+
+def hypotenuse(a, b):
+	return math.hypot(a, b)
+
 def add(a, b):
 	return a + b
 
