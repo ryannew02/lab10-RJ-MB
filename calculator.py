6ddf9fcc1d22ef35246a88c493773c5c137fd358
Ryan Juergens <ryanjuergens@mail.com>
2025-11-10 19:33:42 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -17,15 +17,6 @@ def square_root(a):
 def hypotenuse(a, b):
 	return math.hypot(a, b)
 
-def add(a, b):
-	return a + b
-
-def sub(a, b):
-	return a - b
-
-def mul(a, b):
-	return a * b
-
 def div(a, b):
 	if a == 0:
 		raise ZeroDivisionError
@@ -49,12 +40,6 @@ def sub(a,b):
 def mul(a,b):
     return a * b
 
-def log(a,b):
-    if a <= 0 or b <= 0:
-        raise ValueError
-    math.log(a,b)
 
-def exp(a,b):
-    a ** b
 
 
